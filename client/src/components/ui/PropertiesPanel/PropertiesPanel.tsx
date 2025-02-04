4dde2523943e7a20378d2d9b5c849f325add318e
Fazlurrr <s364696@oslomet.no>
2025-02-04 13:23:54 +0100

    refactor: added scrolling ability in properties panel

--- client/src/components/ui/PropertiesPanel/PropertiesPanel.tsx
+++ client/src/components/ui/PropertiesPanel/PropertiesPanel.tsx
@@ -13,7 +13,13 @@ function isEdgeElement(element: Node | Edge): element is Edge {
 
 const PropertiesPanel: React.FC<PropertiesPanelProps> = ({ selectedElement }) => {
   return (
-    <div className="h-full w-56 border border-[#9facbc] bg-white dark:bg-navbar-dark fixed top-0 right-0 z-10">
+    <div className="h-full w-56 border border-[#9facbc] bg-white dark:bg-navbar-dark fixed top-0 right-0 z-10 
+        overflow-y-auto
+        [&::-webkit-scrollbar]:w-1
+        [&::-webkit-scrollbar-track]:bg-white
+        [&::-webkit-scrollbar-thumb]:bg-gray-200
+        dark:[&::-webkit-scrollbar-track]:bg-neutral-700
+        dark:[&::-webkit-scrollbar-thumb]:bg-neutral-500">
       <div className="p-4 mt-14 mb-2 border-b border-[#9facbc]">
         <h2 className="text-lg font-semibold text-black dark:text-white">Properties</h2>
       </div>
