bb375eb32e682325f7adaf7b5241229ce69bd1ca
eskilschjong <90253352+eskilschjong@users.noreply.github.com>
2025-02-07 10:48:26 +0100

    style: made relation button icons match edge styling. - changed partonomy edge to use diamond shape like in imf manual

--- client/src/components/Edges/Part.tsx
+++ client/src/components/Edges/Part.tsx
@@ -16,37 +16,21 @@ const Part = (props: CustomEdgeProps) => {
     <g onClick={() => openSidebar({ ...props, type: 'part' })}>
       <defs>
         <marker
-          id="parthead"
-          markerWidth="10"
-          markerHeight="7"
-          refX="5"
-          refY="3.5"
-          orient="auto"
-          markerUnits="strokeWidth"
+        id="parthead"
+        markerWidth="8"
+        markerHeight="5.6"
+        refX="8.5"
+        refY="2.8"
+        orient="auto"
+        markerUnits="strokeWidth"
         >
-          <svg
-            className="fill-part"
-            width="7px"
-            height="7px"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              className="stroke-line"
-              d="M11.5757 1.42426C11.81 1.18995 12.1899 1.18995 12.4243 1.42426L22.5757 11.5757C22.81 11.81 22.8101 12.1899 22.5757 12.4243L12.4243 22.5757C12.19 22.81 11.8101 22.8101 11.5757 22.5757L1.42426 12.4243C1.18995 12.19 1.18995 11.8101 1.42426 11.5757L11.5757 1.42426Z"
-              stroke="black"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-          </svg>
+        <polygon points="4.5,0 9,2.8 4.5,5.6 0,2.8" fill="black" />
         </marker>
       </defs>
       <path
         stroke="black"
         strokeWidth="2"
         d={pathData}
-        fill="none"
         markerEnd="url(#parthead)"
       />
     </g>
