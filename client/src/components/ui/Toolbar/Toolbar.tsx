bae1d3f1267d0515734f17da2463dc1cc040e724
eskilschjong <90253352+eskilschjong@users.noreply.github.com>
2025-03-18 10:50:49 +0100

    style: added shared tailwind variables for icons and elements in toolbar. - Improved styling in toolbar

--- client/src/components/ui/Toolbar/Toolbar.tsx
+++ client/src/components/ui/Toolbar/Toolbar.tsx
@@ -1,51 +1,71 @@
 import { Move, Scaling, Workflow, Clipboard, ClipboardPaste,
     Scissors, Trash2, Undo2, Redo2, ZoomIn, ZoomOut, Fullscreen, Lock} from 'lucide-react';
- 
+import { useState } from 'react';
+
 const Toolbar = () => {
+    const [mode, setMode] = useState('move');
+
+    const iconStyle = 'w-5 h-5 ml-1.5 mt-1.5';
+    const iconContainerStyle = 'w-8 h-full hover:text-black dark:hover:text-white mr-2';
+    const activeIconContainerStyle = 'bg-gray-300';
+    const dividerStyle = 'w-0.5 h-5 bg-[#B7C0CD] ml-2 mr-4';
+
     return (
         <div className="fixed top-12 bg-white h-8 w-screen z-20 border-b border-[#9facbc] text-[#61656E] dark:text-white dark:bg-navbar-dark">
-            <div className='flex items-center h-full gap-4 px-4 pl-5'>
-                <div title="Move (V)">
-                    <Move className="w-5 h-5" />
+            <div className='flex items-center h-full px-4 pl-5'>
+                <div 
+                    title="Move (V)" 
+                    onClick={() => setMode('move')} 
+                    className={`${iconContainerStyle} ${mode === 'move' ? activeIconContainerStyle : ''}`}
+                >
+                    <Move className={iconStyle} />
                 </div>
-                <div title="Transform (T)">
-                    <Scaling className="w-5 h-5" />
+                <div 
+                    title="Transform (T)" 
+                    onClick={() => setMode('transform')} 
+                    className={`${iconContainerStyle} ${mode === 'transform' ? activeIconContainerStyle : ''}`}
+                >
+                    <Scaling className={iconStyle} />
                 </div>
-                <div title="Relation (R)">
-                    <Workflow className="w-5 h-5" />
+                <div 
+                    title="Relation (R)" 
+                    onClick={() => setMode('relation')} 
+                    className={`${iconContainerStyle} ${mode === 'relation' ? activeIconContainerStyle : ''}`}
+                >
+                    <Workflow className={iconStyle} />
                 </div>
-                <div className='w-0.5 h-5 bg-[#B7C0CD]'></div>
-                <div title="Copy (Ctrl+C)">
-                    <Clipboard className="w-5 h-5" />
+                <div className={dividerStyle}></div>
+                <div title="Copy (Ctrl+C)" className={iconContainerStyle}>
+                    <Clipboard className={iconStyle} />
                 </div>
-                <div title="Paste (Ctrl+V)">
-                    <ClipboardPaste className="w-5 h-5" />
+                <div title="Paste (Ctrl+V)" className={iconContainerStyle}>
+                    <ClipboardPaste className={iconStyle} />
                 </div>
-                <div title="Cut (Ctrl+X)">
-                    <Scissors className="w-5 h-5" />
+                <div title="Cut (Ctrl+X)" className={iconContainerStyle}>
+                    <Scissors className={iconStyle} />
                 </div>
-                <div title="Delete (Backspace)">
-                    <Trash2 className="w-5 h-5" />
+                <div title="Delete (Backspace)" className={iconContainerStyle}>
+                    <Trash2 className={iconStyle} />
                 </div>
-                <div className='w-0.5 h-5 bg-[#B7C0CD]'></div>
-                <div title="Undo (Ctrl+Z)">
-                    <Undo2 className="w-5 h-5" />
+                <div className={dividerStyle}></div>
+                <div title="Undo (Ctrl+Z)" className={iconContainerStyle}>
+                    <Undo2 className={iconStyle} />
                 </div>
-                <div title="Redo (Ctrl+Shift+Z)">
-                    <Redo2 className="w-5 h-5" />
+                <div title="Redo (Ctrl+Shift+Z)" className={iconContainerStyle}>
+                    <Redo2 className={iconStyle} />
                 </div>
-                <div className='w-0.5 h-5 bg-[#B7C0CD]'></div>
-                <div title="Zoom In (+)">
-                    <ZoomIn className="w-5 h-5" />
+                <div className={dividerStyle}></div>
+                <div title="Zoom In (+)" className={iconContainerStyle}>
+                    <ZoomIn className={iconStyle} />
                 </div>
-                <div title="Zoom Out (-)">
-                    <ZoomOut className="w-5 h-5" />
+                <div title="Zoom Out (-)" className={iconContainerStyle}>
+                    <ZoomOut className={iconStyle} />
                 </div>
-                <div title="Fit View (Ctrl+F)">
-                    <Fullscreen className="w-5 h-5" />
+                <div title="Fit View (Ctrl+F)" className={iconContainerStyle}>
+                    <Fullscreen className={iconStyle} />
                 </div>
-                <div title="Lock Movement (Ctrl+L)">
-                    <Lock className="w-5 h-5" />
+                <div title="Lock Movement (Ctrl+L)" className={iconContainerStyle}>
+                    <Lock className={iconStyle} />
                 </div>
             </div>
         </div>
