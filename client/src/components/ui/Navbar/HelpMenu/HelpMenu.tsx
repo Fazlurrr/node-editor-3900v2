12c55f0d91da07faf454673065c159a7d14bda13
byrjall <filipbyrjall@gmail.com>
2025-05-09 19:32:22 +0200

    feat: add imf guru with defintions

--- client/src/components/ui/Navbar/HelpMenu/HelpMenu.tsx
+++ client/src/components/ui/Navbar/HelpMenu/HelpMenu.tsx
@@ -1,6 +1,8 @@
 import { X } from 'lucide-react';
 import Credits from './Credits';
 import Tutorial from './Tutorial';
+import KeyboardShortcuts from './KeyboardShortcuts';
+import Guru from './Guru';
 import { useTheme } from '@/hooks';
 import { useHotkeys } from 'react-hotkeys-hook';
 
@@ -23,13 +25,19 @@ const HelpMenu: React.FC<HelpMenuProps> = ({close, page}) => {
             />
             <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-1/2 bg-white dark:bg-[#232528] shadow-xl rounded-lg z-50 border border-[#9facbc]">
           <div className='flex justify-between items-center mb-4 p-2 pl-4 border-b border-[#9facbc] font-bold'>
-              {currentPage === 'Tutorial' ? 'Tutorial' : currentPage === 'Credits' ? 'Credits' : ''}
+              {currentPage === 'Tutorial' ? 'Tutorial' : currentPage === 'Credits' ? 'Credits' : currentPage === 'KeyboardShortcuts' ? 'Keyboard Shortcuts' : currentPage === 'Guru' ? 'Guru' : ''}
               <span className="cursor-pointer" title='Close' onClick={close}>
             <X size={18} />
               </span>
           </div>
           {currentPage === 'Credits' && (
               <Credits />
+          )}
+          {currentPage === 'KeyboardShortcuts' && (
+              <KeyboardShortcuts />
+          )}          
+          {currentPage === 'Guru' && (
+              <Guru/>
           )}
           {currentPage === 'Tutorial' && (
               <Tutorial close={close} />
